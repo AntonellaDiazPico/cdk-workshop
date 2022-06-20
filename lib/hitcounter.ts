8b90153308ad334c2fcae4a88bc3d43c4f507531
AntonellaDiazPico <antonelladiazpico@gmail.com>
2022-06-20 15:21:19 +1000

    DynamoDB table encryption and test added

--- lib/hitcounter.ts
+++ lib/hitcounter.ts
@@ -22,7 +22,8 @@ export class HitCounter extends Construct {
       partitionKey: {
         name: "path",
         type: dynamodb.AttributeType.STRING
-      }
+      },
+      encryption: dynamodb.TableEncryption.AWS_MANAGED
     });
     this.table = table;
 
